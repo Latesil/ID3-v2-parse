53ca881d527ff152e44bac4b09bcef0b30b85a22
Latesil <skipastas@gmail.com>
2018-07-24 16:05:51 +0300

    vers2

--- bin.py
+++ bin.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import sys
 
 def sys_exit(msg):
@@ -10,18 +12,39 @@ if len(sys.argv) > 2:
 if len(sys.argv) == 1:
     sys_exit("Need at least one file to open")
 
-tags = [b'TYER', b'TRCK', b'AENC', b'ASPI', b'APIC', b'COMM', b'COMR', b'ENCR',
-        b'EQUA', b'EQU2', b'ETCO', b'GEOB', b'GRID', b'IPLS', b'TIPL', b'LINK',
-        b'MCDI', b'MLLT', b'OWNE', b'PRIV', b'PCNT', b'POPM', b'POSS', b'RBUF', 
-        b'RVAD', b'RVA2', b'RVRB', b'SEEK', b'SIGN', b'SYLT', b'SYTC', b'TALB', 
-        b'TBPM', b'TCOM', b'TCON', b'TCOP', b'TDAT', b'TDRC', b'TDEN', b'TDLY', 
-        b'TDRC', b'TDRL', b'TDTG', b'TENC', b'TEXT', b'TFLT', b'TIME', b'TDRC', 
-        b'TIT1', b'TIT2', b'TIT3', b'TKEY', b'TLAN', b'TLEN', b'TMCL', b'TMED', 
-        b'TMOO', b'TOAL', b'TOFN', b'TOLY', b'TOPE', b'TORY', b'TDOR', b'TOWN', 
-        b'TPE1', b'TPE2', b'TPE3', b'TPE4', b'TPOS', b'TPRO', b'TPUB', b'TXXX', 
-        b'TRDA', b'TDRC', b'TRSN', b'TRSO', b'TSIZ', b'TSOA', b'TSOP', b'TSOT', 
-        b'TSRC', b'TSSE', b'TSST', b'UFID', b'USER', b'USLT', b'WCOM', b'WCOP', 
-        b'WOAF', b'WOAR', b'WOAS', b'WORS', b'WPAY', b'WPUB', b'WXXX', b'CTPE']
+tags = {b'TYER': "Year", b'TRCK': 'Track number', 
+        b'AENC': 'Info', b'ASPI': 'Info', b'APIC': 'Info', 
+        b'COMM': 'Comment', b'COMR': 'Info', b'ENCR': 'Info',
+        b'EQUA': 'Info', b'EQU2': 'Info', b'ETCO': 'Info', 
+        b'GEOB': 'Info', b'GRID': 'Info', b'IPLS': 'Info', 
+        b'TIPL': 'Info', b'LINK': 'Info', b'MCDI': 'Info', 
+        b'MLLT': 'Info', b'OWNE': 'Info', b'PRIV': 'Info', 
+        b'PCNT': 'Info', b'POPM': 'Info', b'POSS': 'Info', 
+        b'RBUF': 'Info', b'RVAD': 'Info', b'RVA2': 'Info', 
+        b'RVRB': 'Info', b'SEEK': 'Info', b'SIGN': 'Info', 
+        b'SYLT': 'Info', b'SYTC': 'Info', b'TALB':'Album', 
+        b'TBPM': 'Track BPM', b'TCOM': 'Info', b'TCON': 'Genre', 
+        b'TCOP': 'Info', b'TDAT': 'Info', b'TDRC': 'Recording Time', 
+        b'TDEN': 'Info', b'TDLY': 'Info', b'TDRL': 'Info', 
+        b'TDTG': 'Info', b'TENC': 'Info', 
+        b'TEXT': 'Info', b'TFLT': 'Info', b'TIME': 'Info', 
+        b'TIT1': 'Info', b'TIT2': 'Title', 
+        b'TIT3': 'Info', b'TKEY': 'Info', b'TLAN': 'Info', 
+        b'TLEN': 'Info', b'TMCL': 'Info', b'TMED': 'Info', 
+        b'TMOO': 'Info', b'TOAL': 'Info', b'TOFN': 'Info', 
+        b'TOLY': 'Info', b'TOPE': 'Info', b'TORY': 'Info', 
+        b'TDOR': 'Info', b'TOWN': 'Info', b'TPE1': 'Artist', 
+        b'TPE2':'Artist', b'TPE3': 'Artist', b'TPE4': 'Artist', 
+        b'TPOS': 'Info', b'TPRO': 'Info', b'TPUB': 'Info', 
+        b'TXXX': 'Info', b'TRDA': 'Info', 
+        b'TRSN': 'Info', b'TRSO': 'Info', b'TSIZ': 'Info', 
+        b'TSOA': 'Info', b'TSOP': 'Info', b'TSOT': 'Info', 
+        b'TSRC': 'Info', b'TSSE': 'Info', b'TSST': 'Info', 
+        b'UFID': 'Info', b'USER': 'Info', b'USLT': 'Info', 
+        b'WCOM': 'Info', b'WCOP': 'Info', b'WOAF': 'Info', 
+        b'WOAR': 'Info', b'WOAS': 'Info', b'WORS': 'Info', 
+        b'WPAY': 'Info', b'WPUB': 'Info', b'WXXX': 'Info', 
+        b'CTPE': 'Info'}
 
 found_tags = {}
 def main():
@@ -49,7 +72,7 @@ def main():
                 binary_file.seek(-1, 1)
                 tag = binary_file.read(4)
                 size = int.from_bytes(binary_file.read(4), byteorder='big')
-                if tag in tags:
+                if tag in tags.keys():
                     
                     collect_tag(binary_file, start)
                     start += size + header_size
@@ -60,7 +83,7 @@ def main():
                 binary_file.seek(-1, 1)
                 tag = binary_file.read(4)
                 size = int.from_bytes(binary_file.read(4), byteorder='big')
-                if tag in tags:
+                if tag in tags.keys():
                     
                     collect_tag(binary_file, start)
                     start += size + header_size
@@ -79,6 +102,7 @@ def main():
 def collect_tag(bin, offset):
     bin.seek(offset, 0)
     tag1 = bin.read(4)
+    # tag_description = found_tags[tag1]
     size = int.from_bytes(bin.read(4), byteorder='big')
     flag_bytes = bin.read(2)
 
@@ -91,6 +115,8 @@ def collect_tag(bin, offset):
     while count != size:
         b += bin.read(1)
         count += 1
+        if count > 200:
+            break
     
     found_tags[tag1.decode()] = b
     return
@@ -102,7 +128,7 @@ def parse_tags(d):
                 decoding_text = 'ISO-8859-1'
                 offset = 0
 
-            if value[0] == 1:
+            if value[0] == 1 or value[0] == 2:
                 if value[1] == 255:
                     if value[2] == 254:
                         decoding_text = 'utf-16-le'
@@ -111,25 +137,15 @@ def parse_tags(d):
                 if value[1] == 254:
                     if value[2] == 255:
                         decoding_text = 'utf-16-be'
-                        offset = 3
-            
-            if value[0] == 2:
-                if value[1] == 255:
-                    if value[2] == 254:
-                        decoding_text = 'utf-16-le'
-                        offset = 3
-
-                if value[1] == 254:
-                    if value[2] == 255:
-                        decoding_text = 'utf-16-be'
-                        offset = 3
 
 
             if value[0] == 3:
                 decoding_text = 'utf-8'
                 offset = 3
 
-            print("{0}: {1}".format(key, value[offset:].decode(decoding_text)))
+            description = tags[str.encode(key)]
+            # print("{0}: {1}".format(key, value[offset:].decode(decoding_text)))
+            print("{0}: {1}".format(description, value[offset:].decode(decoding_text)))
 
         if key[0] == "C":
             if value[0] == 1: # find language
@@ -142,18 +158,7 @@ def parse_tags(d):
                 decoding_text = 'ISO-8859-1'
                 offset = 0
 
-            if value[0] == 1:
-                if value[1] == 255:
-                    if value[2] == 254:
-                        decoding_text = 'utf-16-le'
-                        offset = 3
-
-                if value[1] == 254:
-                    if value[2] == 255:
-                        decoding_text = 'utf-16-be'
-                        offset = 3
-            
-            if value[0] == 2:
+            if value[0] == 1 or value[0] == 2:
                 if value[1] == 255:
                     if value[2] == 254:
                         decoding_text = 'utf-16-le'
@@ -167,8 +172,8 @@ def parse_tags(d):
             if value[6] == 0:
                 if value[7] == 0:
                     pass
-            
-            print("{0}: {1}".format(key, value[offset * 2:].decode(decoding_text)))
+            description = tags[str.encode(key)]
+            print("{0}: {1}".format(description, value[offset * 2:].decode(decoding_text)))
 
         if key[0] == "W":
             print("{0}: {1}".format(key, value))
